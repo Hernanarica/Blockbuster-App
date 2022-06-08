c704ad1e6ca2b5808f29c82faad8663e041112dd
Hernán Arica <hernanaricammm@gmail.com>
2022-06-07 23:16:00 -0300

    Se realizo el login con cuenta de google y email con password

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyCdus1r5EC_HvlfGockEKYXcjlNVlMtmMo",
@@ -10,5 +10,6 @@ const firebaseConfig = {
 	appId: "1:694973347526:web:6e7fe9e6e015c721cac9c7"
 };
 
-const app         = initializeApp(firebaseConfig);
-export const auth = getAuth();
+const app                   = initializeApp(firebaseConfig);
+export const auth           = getAuth();
+export const googleProvider = new GoogleAuthProvider();
--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,11 +1,43 @@
 import { Link } from "react-router-dom";
+import { signInWithPopup, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth, googleProvider } from "../../firebase/firebase.config";
 import getImagePath from "../../helpers/getImagePath";
 
 function LoginScreen() {
+	const user = {
+		email: 'hernommm@gmail.com',
+		password: 'asdf1234'
+	};
+	
+	const handleSubmit = e => {
+		e.preventDefault();
+		console.log('Login...');
+		
+		signInWithEmailAndPassword(auth, user.email, user.password)
+			.then(r => {
+				console.log(r);
+			}).catch(err => {
+			console.log(err);
+		});
+	};
+	
+	const handleLoginWithGoogle = () => {
+		signInWithPopup(auth, googleProvider)
+			.then(r => {
+				console.log(r);
+			}).catch((error) => {
+			console.log(error);
+		});
+	};
+	
+	
 	return (
 		<section className="wrapper login-section">
 			<h2 className="form__title">Inicia sesión</h2>
-			<form action="" method="post" encType="multipart/form-data" className="login__form">
+			<form action="#"
+			      className="login__form"
+			      onSubmit={ handleSubmit }
+			>
 				<div className="login__form-content">
 					<label htmlFor="email" className="login__form-field-title">Ingresa tu email</label>
 					<input type="email" name="email" className="login__form-field" id="email" placeholder="Ingresa tu email" autoComplete="off" />
@@ -17,11 +49,10 @@ function LoginScreen() {
 				<button type="submit" className="btn btn--active">
 					Iniciar sesión
 				</button>
-				<button type="submit" className="btn">
+				<button className="btn" onClick={ handleLoginWithGoogle }>
 					<img src={ getImagePath('login-google.png') } alt="Google" />
 				</button>
-				<p className="helps__text">¿Aún no tenés una cuenta?
-					<Link to="/register" className="helps__link">Registrate</Link>
+				<p className="helps__text">¿Aún no tenés una cuenta? <Link to="/register" className="helps__link">Registrate</Link>
 				</p>
 			</form>
 		</section>
